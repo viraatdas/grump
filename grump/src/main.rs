7e9453d0c517bdc3e659bc3d29073f08f656442a
Viraat <viraat.laldas@gmail.com>
2025-02-06 04:17:23 -0800

    main

--- grump/src/main.rs
+++ grump/src/main.rs
@@ -1,3 +1,19 @@
+mod lexer;
+mod parser;
+mod token;
+mod wasm;
+
 fn main() {
-    println!("Hello, world!");
+    let input = "(+ 10 20)";
+    let mut lexer = lexer::Lexer::new(input);
+    let tokens = lexer.tokenize();
+
+    let mut parser = parser::Parser::new(tokens);
+    let ast = parser.parse();
+
+    println!("Parsed AST: {:?}", ast);
+
+    let wasm_code = wasm::generate_wasm();
+    std::fs::write("output.wasm", wasm_code).expect("Failed to write Wasm file");
+    println!("Compiled to WebAssembly: output.wasm");
 }
